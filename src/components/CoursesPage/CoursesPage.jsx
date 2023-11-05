806bb3e1a30ff4538c8a42818d668c93d5f034ec
Rayan Obeid <rayan.obeid93@gmail.com>
2023-11-05 14:54:57 +0200

    add Courses Page

--- src/components/CoursesPage/CoursesPage.jsx
+++ src/components/CoursesPage/CoursesPage.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Teachers from '../Teachers/Teachers';
 import TrainingProgram from '../TraininProgram/TrainingProgram';
 import Price from '../Price/Price';
+import ProjectStudent from '../ProjectStudent/ProjectStudent';
 
 
 
 function CoursesPage() {
   return (
     <div>
-        <Teachers/>
-        <TrainingProgram/>
-        <Price/>
-        
+      <ProjectStudent />
+      <TrainingProgram />
+      <Teachers />
+      <Price />
+
+
     </div>
   )
 }
