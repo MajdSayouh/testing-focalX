4b7449aa495a17a81f9606f30800a3ff1e11cab6
MahaAlchommari <mahaalchommari@gmail.com>
2023-11-08 02:34:16 +0300

    task

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -7,149 +7,82 @@ import img95 from "./../../assets/img/image4.png";
 import img94 from "./../../assets/img/unsplash_yJwbvWmJs5M.png";
 
 import "./Cards.css";
+import CardsContent from "../CardsContent/CardsContent";
 const Cards = () => {
   return (
     <section className="container">
-      <div className=" d-flex ma-nav2-card gap-5 flex-wrap">
-        <img src={img100} alt="" />
-        <div className="ma-card-package">
-          <div className="ma-nav2-underline"></div>
-          <h2>10 updated Figma features in 2022</h2>
-          <a href="" className=" text-decoration-none">
-            # UX/UI Design
-          </a>
-          <p className="ma-nav2-p">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis occaecati cupiditate non provident, similique sunt in
-            culpa qui officia deserunt mollitia animi, id est laborum et dolorum
-            fuga...
-          </p>
-          <div className=" d-flex align-items-center justify-content-between flex-wrap ma-worp">
-            <div className=" d-flex align-items-center gap-2 flex-wrap ma-clock">
-              <img src={img99} alt="" className=" mb-3" />
-              <p className=" text-black-50">3 min read</p>
-            </div>
-            <p className=" text-black-50">April 12, 2022</p>
-          </div>
-        </div>
-      </div>
-      <div className=" d-flex ma-nav2-card gap-5 flex-wrap">
-        <img src={img98} alt="" />
-        <div className="ma-card-package">
-          <div className="ma-nav2-underline"></div>
-          <h2>What new programming languages are in demand now?</h2>
-          <a href="" className=" text-decoration-none">
-            # Programming
-          </a>
-          <p className="ma-nav2-p">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis occaecati cupiditate non provident, similique sunt in
-            culpa qui officia deserunt mollitia animi, id est laborum et dolorum
-            fuga...
-          </p>
-          <div className=" d-flex align-items-center justify-content-between flex-wrap ma-worp">
-            <div className=" d-flex align-items-center gap-2 flex-wrap ma-clock">
-              <img src={img99} alt="" className=" mb-3" />
-              <p className=" text-black-50">5 min read</p>
-            </div>
-            <p className=" text-black-50">March 25, 2022</p>
-          </div>
-        </div>
-      </div>
-      <div className=" d-flex ma-nav2-card gap-5 flex-wrap">
-        <img src={img97} alt="" />
-        <div className="ma-card-package">
-          <div className="ma-nav2-underline"></div>
-          <h2>Top popular styles in illustration 2022</h2>
-          <a href="" className=" text-decoration-none">
-            # illustration
-          </a>
-          <p className="ma-nav2-p">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis occaecati cupiditate non provident, similique sunt in
-            culpa qui officia deserunt mollitia animi, id est laborum et dolorum
-            fuga...
-          </p>
-          <div className=" d-flex align-items-center justify-content-between flex-wrap ma-worp">
-            <div className=" d-flex align-items-center gap-2 flex-wrap ma-clock">
-              <img src={img99} alt="" className=" mb-3" />
-              <p className=" text-black-50">7 min read</p>
-            </div>
-            <p className=" text-black-50">March 18, 2022</p>
-          </div>
-        </div>
-      </div>
-      <div className=" d-flex ma-nav2-card gap-5 flex-wrap">
-        <img src={img96} alt="" />
-        <div className="ma-card-package">
-          <div className="ma-nav2-underline"></div>
-          <h2>
-            The secret of a successful frame. What rules should be followed?
-          </h2>
-          <a href="" className=" text-decoration-none">
-            # Photo & Film
-          </a>
-          <p className="ma-nav2-p">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis occaecati cupiditate non provident, similique sunt in
-            culpa qui officia deserunt mollitia animi, id est laborum et dolorum
-            fuga...
-          </p>
-          <div className=" d-flex align-items-center justify-content-between flex-wrap ma-worp">
-            <div className=" d-flex align-items-center gap-2 flex-wrap ma-clock">
-              <img src={img99} alt="" className=" mb-3" />
-              <p className=" text-black-50">6 min read</p>
-            </div>
-            <p className=" text-black-50">March 17, 2022</p>
-          </div>
-        </div>
-      </div>
-      <div className="container d-flex ma-nav2-card gap-5 flex-wrap">
-        <img src={img95} alt="" />
-        <div className="ma-card-package">
-          <div className="ma-nav2-underline"></div>
-          <h2>Brainstorming in design: from the idea to its implementation</h2>
-          <a href="" className=" text-decoration-none">
-            # UX/UI Design
-          </a>
-          <p className="ma-nav2-p">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis occaecati cupiditate non provident, similique sunt in
-            culpa qui officia deserunt mollitia animi, id est laborum et dolorum
-            fuga...
-          </p>
-          <div className=" d-flex align-items-center justify-content-between flex-wrap ma-worp">
-            <div className=" d-flex align-items-center gap-2 flex-wrap ma-clock">
-              <img src={img99} alt="" className=" mb-3" />
-              <p className=" text-black-50">12 min read</p>
-            </div>
-            <p className=" text-black-50">March 12, 2022</p>
-          </div>
-        </div>
-      </div>
-      <div className=" d-flex ma-nav2-card gap-5 flex-wrap">
-        <img src={img94} alt="" />
-        <div className="ma-card-package">
-          <div className="ma-nav2-underline"></div>
-          <h2>Who is an Internet marketer and what does he do?</h2>
-          <a href="" className=" text-decoration-none">
-            # Marketing
-          </a>
-          <p className="ma-nav2-p">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis occaecati cupiditate non provident, similique sunt in
-            culpa qui officia deserunt mollitia animi, id est laborum et dolorum
-            fuga...
-          </p>
-          <div className="ma-worp d-flex align-items-center justify-content-between flex-wrap ">
-            <div className=" d-flex align-items-center gap-2 flex-wrap ma-clock">
-              <img src={img99} alt="" className=" mb-3" />
-              <p className=" text-black-50">5 min read</p>
-            </div>
-            <p className=" text-black-50">February 2, 2022</p>
-          </div>
-        </div>
-      </div>
+      <CardsContent
+        img100={img100}
+        h2="10 updated Figma features in 2022"
+        a="# UX/UI Design"
+        p="At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis occaecati cupiditate non provident, similique sunt in
+        culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+        fuga..."
+        img99={img99}
+        p2="3 min read"
+        p3="April 12, 2022"
+      />
+      <CardsContent
+        img100={img98}
+        h2="What new programming languages are in demand now?"
+        a="# Programming"
+        p="At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis occaecati cupiditate non provident, similique sunt in
+        culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+        fuga..."
+        img99={img99}
+        p2="5 min read"
+        p3="March 25, 2022"
+      />
+      <CardsContent
+        img100={img97}
+        h2="Top popular styles in illustration 2022"
+        a="# illustration"
+        p="At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis occaecati cupiditate non provident, similique sunt in
+        culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+        fuga..."
+        img99={img99}
+        p2="7 min read"
+        p3="March 18, 2022"
+      />
+      <CardsContent
+        img100={img96}
+        h2="The secret of a successful frame. What rules should be followed?"
+        a="# Photo & Film"
+        p="At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis occaecati cupiditate non provident, similique sunt in
+        culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+        fuga..."
+        img99={img99}
+        p2="6 min read"
+        p3="March 17, 2022"
+      />
+      <CardsContent
+        img100={img95}
+        h2="Brainstorming in design: from the idea to its implementation"
+        a="# UX/UI Design"
+        p="At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis occaecati cupiditate non provident, similique sunt in
+        culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+        fuga..."
+        img99={img99}
+        p2="12 min read"
+        p3="March 12, 2022"
+      />
+      <CardsContent
+        img100={img94}
+        h2="Who is an Internet marketer and what does he do?"
+        a="# Marketing"
+        p="At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis occaecati cupiditate non provident, similique sunt in
+        culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+        fuga..."
+        img99={img99}
+        p2="5 min read"
+        p3="February 2, 2022"
+      />
     </section>
   );
 };
